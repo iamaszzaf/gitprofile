55e0986af5c73f5688f276b9f3f281a366056189
Łukasz Majka <majkalukasz07@gmail.com>
2025-07-18 18:57:24 +0200

    Update index.tsx

--- src/components/external-project-card/index.tsx
+++ src/components/external-project-card/index.tsx
@@ -151,7 +151,7 @@ const ExternalProjectCard = ({
                   <div className="text-base-content/60 text-xs sm:text-sm mt-1 truncate">
                     {loading
                       ? skeleton({ widthCls: 'w-32', heightCls: 'h-4' })
-                        : `Znaleziono ${externalProjects.length} ${externalProjects.length === 1 ? 'element' : (externalProjects.length < 5 ? 'elementy' : 'elementó')}.`}
+                        : `Znaleziono ${externalProjects.length} ${externalProjects.length === 1 ? 'projekt' : (externalProjects.length < 5 ? 'projekty' : 'projektów')}.`}
                   </div>
                 </div>
               </div>
